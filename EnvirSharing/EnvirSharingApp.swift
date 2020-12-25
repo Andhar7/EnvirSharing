421b6f4f8db72e5443d7365f330b544b28a292c4
Andhar7 <70223638+Andhar7@users.noreply.github.com>
2020-12-25 20:39:48 +0100

    Initial Commit

---
+++ EnvirSharing/EnvirSharingApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvirSharingApp.swift
+//  EnvirSharing
+//
+//  Created by Andrej Kling on 25.12.20.
+//
+
+import SwiftUI
+
+@main
+struct EnvirSharingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
